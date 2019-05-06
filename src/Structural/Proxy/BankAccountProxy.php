9f958189e078dd7df922e0073e30956f67e1af7a
VovaIvzhenko <ivzhen4@gmail.com>
2019-05-06 18:30:24 +0300

    Implemented BankAccountProxy

---
+++ src/Structural/Proxy/BankAccountProxy.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivzhenko.volodymyr
+ * Date: 06.05.2019
+ * Time: 17:59
+ */
+
+namespace Structural\Proxy;
+
+
+/**
+ * Class BankAccountInterfaceProxy
+ * @package Structural\Proxy
+ */
+class BankAccountProxy extends HeavyBankAccountInterface implements BankAccountInterface
+{
+	/**
+	 * @var
+	 */
+	private $balance;
+
+	/**
+	 * @return int
+	 */
+	public function getBalance(): int
+	{
+		if ($this->balance === null) {
+			$this->balance = parent::getBalance();
+		}
+
+		return $this->balance;
+	}
+}
